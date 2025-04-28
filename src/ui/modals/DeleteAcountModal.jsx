c24536dd17c70915c9b58f9741f33bfa577daaa2
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-04-28 13:18:44 +0300

    feat: Add delete account functionality with confirmation modal
    - Updated the UserMenu component to include a link for deleting the account, which triggers a confirmation modal.
    - Created a new DeleteAcountModal component to handle the confirmation process for account deletion.
    - Added styles for the delete button and modal in the SCSS files to enhance UI consistency.
    - Adjusted existing styles in _config.scss and _layout.scss to accommodate new components and improve overall layout.

---
+++ src/ui/modals/DeleteAcountModal.jsx
@@ -0,0 +1,38 @@
+import { Modal } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
+import SubmitButton from "./../forms/SubmitButton";
+
+function DeleteAcountModal({
+  setShowModal,
+  showModal,
+  eventFunction,
+  loading,
+}) {
+  const { t } = useTranslation();
+
+  return (
+    <Modal show={showModal} onHide={() => setShowModal(false)} centered>
+      <Modal.Header className="pb-0" closeButton />
+      <Modal.Body className="pay_modal gap-2">
+        <h3 className="text-end">
+          {t("auth.areYouSureYouWantToDeleteYourAccount")}{" "}
+        </h3>
+
+        <div className="d-flex justify-content-end gap-3 mt-4">
+          <button onClick={() => setShowModal(false)} className="cancel-btn">
+            {t("cancel")}
+          </button>
+
+          <SubmitButton
+            name={t("auth.confirm")}
+            loading={loading}
+            onClick={eventFunction}
+            className={"delete-btn"}
+          />
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+export default DeleteAcountModal;
